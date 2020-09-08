4ca3a91e34c360d760267c86906cec28c35790ac
Lee-Geon-Yeong <yellowisher@naver.com>
2020-09-08 09:32:22 +0900

    fixed gitignore

--- cplusplus_project/src/main.cpp
+++ cplusplus_project/src/main.cpp
@@ -3,9 +3,15 @@
 #include "Car.hpp"
 #include "PrintData.hpp"
 using namespace std;
+void printCar(Car car ){
+    cout<< car.getSpeed()<<endl;
+    car.setSpeed(20);
+}
+
 int main(int argc, char const *argv[]) {
     Car myCar;
     myCar.setSpeed(100);
+    printCar(myCar); // call method : call by value
     cout<<"speed:"<<myCar.getSpeed()<<endl;
 
     PrintData pd;
